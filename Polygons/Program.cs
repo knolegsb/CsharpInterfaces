f5a0509d1b09b777bf94383d12c580f02aa9af2b
knolegsb <seon2you@hotmail.com>
2015-10-11 16:31:21 -0700

    start

---
+++ Polygons/Program.cs
@@ -0,0 +1,23 @@
+﻿using Polygons.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+        }
+
+        private static void DisplayPolygon(string polygonType, Square polygon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
